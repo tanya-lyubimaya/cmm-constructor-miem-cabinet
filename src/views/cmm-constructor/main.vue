5638d0c100b15d653a0f08530c0547028c7f1cd3
shorygina <teshorygina@edu.hse.ru>
2020-07-06 15:53:14 +0300

    Removed styles, added form value to message

--- src/views/cmm-constructor/main.vue
+++ src/views/cmm-constructor/main.vue
@@ -12,9 +12,13 @@
             <a class="button button-outline" href="javascript:flipflop('enterNameOfCMM');">Создать КИМ</a>
 
             <div id="enterNameOfCMM">
-              <el-form ref="form" :model="form" label-width="120px">
+              <el-form ref="form" :model="form" label-width="120px" id="cmm-name-form">
                   <el-form-item label="Название КИМа">
-                    <el-input v-model="form.name" />
+                    <el-input v-model="form.name" class="input" name="cmm-name"/>
+                  </el-form-item>
+                  <el-form-item>
+                    <el-button type="primary" native-type="submit" @click="onSubmit">Create</el-button>
+                    <el-button @click="onCancel">Cancel</el-button>
                   </el-form-item>
               </el-form>
             <!--
@@ -44,128 +48,20 @@
   </div>
 </template>
 
-<style>
-    .wrapper {
-    display: block;
-    overflow: hidden;
-    overflow-x: hidden;
-    overflow-y: hidden;
-    position: relative;
-    width: 100%;
-}
-    .navigation {
-        background: #f4f5f6;
-        border-bottom: .1rem solid #d1d1d1;
-        display: block;
-        height: 5.2rem;
-        left: 0;
-        max-width: 100%;
-        position: fixed;
-        right: 0;
-        top: 0;
-        width: 100%;
-        z-index: 1;
-    }
-    .navigation .navigation-link, .navigation .navigation-title, .navigation .title {
-        display: inline;
-        font-size: 1.6rem;
-        line-height: 5.2rem;
-        padding: 0;
-        text-decoration: none;
-        margin: 30px;
-    }
-    .navigation .navigation-title, .navigation .title {
-        color: #606c76;
-        font-family: Roboto,sans-serif;
-        position: relative;
-        font-size: 2rem;
-    }
-    a {
-        color: #9b4dca;
-        text-decoration: none;
-    }
-    p {
-        margin-top: 0;
-    }
-        .header .button {
-        margin-bottom: 4rem;
-        margin-top: 2rem;
-    }
-    .header .img path {
-        animation: 7s a forwards;
-        fill: #9b4dca;
-        stroke: #9b4dca;
-        stroke-dasharray: 38321;
-        stroke-miterlimit: 10;
-        stroke-width: 2px;
-    }
-    .wrapper>.container {
-        padding-bottom: 7.5rem;
-        padding-top: 7.5rem;
-    }
-    #base {
-        max-width: 100rem;
-    }
-    .container {
-        margin: 0 auto;
-        padding: 0 2.0rem;
-        position: relative;
-        width: 100%;
-    }
-    h3 {
-        font-size: 2.8rem;
-        line-height: 1.3;
-    }
-    .button-clear {
-        font-size: 0.9rem;
-        height: 2.8rem;
-        line-height: 2.8rem;
-        padding: 0 2.3rem;
-    }
-    .footer {
-        padding: 8rem 0 2rem;
-    }
-    .footer .copyright {
-        padding-right: 7rem;
-        padding-left: 7rem;
-    }
-    .footer .nav {
-        position: absolute;
-        right: 2rem;
-        top: 50%;
-        transform: translateY(-50%);
-    }
-    .footer .nav .link {
-        display: inline-block;
-        margin-left: 1.2rem;
-        padding: 0;
-    }
-    .footer .nav .icon {
-        fill: #9b4dca;
-        height: 2rem;
-    }
-</style>
-
-
 <script>
 export default {
   data() {
     return {
       form: {
         name: '',
-        region: '',
-        date1: '',
-        date2: '',
-        delivery: false,
-        type: [],
-        resource: '',
-        desc: ''
       }
     }
   },
   methods: {
     onSubmit() {
-      this.$message('submit!')
+      //this.$message('submit!')
+      //this.$http.post('/api/something', JSON.stringify(this.form));
+      this.$message(JSON.stringify(this.form.name))
     },
     onCancel() {
       this.$message({

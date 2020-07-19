4f94e077d51b841b5d10fc7190b88b0ef66228ce
shorygina <teshorygina@edu.hse.ru>
2020-07-19 19:49:20 +0300

    Moved python scripts to mock folder

--- src/views/cmm-constructor/main.vue
+++ src/views/cmm-constructor/main.vue
@@ -140,9 +140,3 @@ created() {
   }
 }
 </script>
-
-<!--
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-<script src="http://code.jquery.com/jquery-2.0.2.min.js"></script>
-<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
--->
